075a6ff6759d793581f1def9d98b8fbd688058bb
Cornellius Ngondo <devngondo@gmail.com>
2015-04-11 18:38:40 +0000

    Initial commit after implementing the material Navigation drawer

---
+++ app/src/main/java/tales/example/ngondo/africatales/Home.java
@@ -0,0 +1,54 @@
+package tales.example.ngondo.africatales;
+
+import android.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import it.neokree.materialnavigationdrawer.MaterialNavigationDrawer;
+import it.neokree.materialnavigationdrawer.elements.MaterialSection;
+import it.neokree.materialnavigationdrawer.elements.listeners.MaterialSectionListener;
+
+
+public class Home extends MaterialNavigationDrawer {
+
+    @Override
+    public void init(Bundle savedInstanceState) {
+
+        //Adding sections to the navigation drawer
+        this.addSection(newSection("Story 1", new Story1()));
+
+
+
+    }
+
+   /* @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }*/
+
+
+    /*@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }*/
+}
